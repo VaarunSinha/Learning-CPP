bc87fa7923ead4d20ebca8c749da8eaba37407a7
Dev2212 <developervaarun@gmail.com>
2022-05-30 10:08:31 +0530

    Restore test to it's orginal version ✅

--- test.cpp
+++ test.cpp
@@ -4,12 +4,6 @@
 
 using namespace std;
 int main(int argc, char* argv[]){
-    string sMiles;
-    double dMiles, dKilometers;
-    cout << "Enter Miles: ";
-    getline(cin, sMiles);
-    dMiles = stod(sMiles);
-    dKilometers = dMiles * 1.60934;
-    printf("%.1f Miles equals %.4f kilometers\n", dMiles, dKilometers);
-    return 0;
+   cout << "Hello World, This Is Your God Speaking." << endl;
+   return 0;
 }
